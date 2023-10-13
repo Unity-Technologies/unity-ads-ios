960bcb1ba5e8e0890fdf27d3a6bc47de1cbf696f
runner <runner@Mac-1697239349757.local>
2023-10-13 23:25:52 +0000

    Release 4.9.1

--- SourceCode/Private/Core/Properties/USRVSdkProperties.m
+++ SourceCode/Private/Core/Properties/USRVSdkProperties.m
@@ -11,12 +11,12 @@ NSString *const kUnityServicesLocalCacheFilePrefix = @"UnityAdsCache-";
 NSString *const kUnityServicesLocalStorageFilePrefix = @"UnityAdsStorage-";
 NSString *const kUnityServicesWebviewBranchInfoDictionaryKey = @"UADSWebviewBranch";
 NSString *const kUnityServicesWebviewConfigInfoDictionaryKey = @"UADSWebviewConfig";
-NSString *const kUnityServicesVersionName = @"4.9.0";
+NSString *const kUnityServicesVersionName = @"4.9.1";
 NSString *const kUnityServicesFlavorDebug = @"debug";
 NSString *const kUnityServicesFlavorRelease = @"release";
 NSString *const kChinaIsoAlpha2Code = @"CN";
 NSString *const kChinaIsoAlpha3Code = @"CHN";
-int const kUnityServicesVersionCode = 4900;
+int const kUnityServicesVersionCode = 4910;
 
 @implementation USRVSdkProperties
 
